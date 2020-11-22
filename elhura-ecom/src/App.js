cb71429e37f22089dfb4da084a6526d1386b67fd
OTHMANE ELGOUBI <othmanelgoubi@gmail.com>
2020-11-22 11:07:16 +0100

    adding collapse side bar with navigation

--- elhura-ecom/src/App.js
+++ elhura-ecom/src/App.js
@@ -1,10 +1,24 @@
-import Navbar from './components/Navbar/Navbar'
+  
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
